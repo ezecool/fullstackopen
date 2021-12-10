53d8dc20ae6a3bfd56396dc4dc20a2e907caca18
Hernan Castro Abregu <castroabreguhernan@gmail.com>
2021-12-10 00:45:55 -0300

    in progress

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,30 +13,60 @@ const Button = ({text, action}) => {
   )
 }
 
-const Statistics = () => {
+const Statistics = ( {good, neutral, bad, total} ) => {
+
+  const average = isNaN((good - bad) / total) ? 0 : (good - bad) / total
+  const positive = isNaN(good / total) ? 0 : (good / total)
+
   return (
     <>
       <h4>Statistics</h4>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
     </>
   )
 }
 
 const App = () => {
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [totalClicks, setTotalClicks] = useState(0)
+
   const title = "Give Feedback"
+
+  const handleGoodClick = () => {
+    setTotalClicks(totalClicks + 1)
+    setGood(good + 1);
+  }
+  
+  const handleNeutralClick = () => {
+    setTotalClicks(totalClicks + 1)
+    setNeutral(neutral + 1);
+  }
+  
+  const handleBadClick = () => {
+    setTotalClicks(totalClicks + 1)
+    setBad(bad + 1);
+  }
 
   return (
     <>
+      
       <Title title={title}/>
-      <Button text="Good" action=/>
-      <Button text="Neutral"/>
-      <Button text="Bad"/>
+      
+      <Button text="Good" action={handleGoodClick}/>
+      <Button text="Neutral" action={handleNeutralClick}/>
+      <Button text="Bad"action={handleBadClick}/>
 
-      <Statistics />
+      <Statistics good={good} neutral={neutral} bad={bad} total={totalClicks}/>
 
     </>
   )
